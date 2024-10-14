260e0e405975f6b8f4f479f07f2535a33b20ba4e
Adarsh <gooneriitk@gmail.com>
2024-10-14 09:49:38 +0530

    configured min and max sleep durations

--- main.go
+++ main.go
@@ -5,25 +5,56 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 
 	sinksdk "github.com/numaproj/numaflow-go/pkg/sinker"
 )
 
 // Slow sink implementation to test metric data in debugging
-type SlowSink struct{}
+type SlowSink struct {
+	minSleepDuration int
+	maxSleepDuration int
+	startTime        time.Time
+}
 
 func newSlowSink() *SlowSink {
-	return &SlowSink{}
+
+	minSleepDurationStr, ok := os.LookupEnv("MIN_SLEEP_TIME")
+	if !ok {
+		minSleepDurationStr = "10"
+	}
+	maxSleepDurationStr, ok := os.LookupEnv("MAX_SLEEP_TIME")
+	if !ok {
+		maxSleepDurationStr = "20"
+	}
+	minSleepDuration, _ := strconv.Atoi(minSleepDurationStr)
+	maxSleepDuration, _ := strconv.Atoi(maxSleepDurationStr)
+	return &SlowSink{
+		minSleepDuration: minSleepDuration,
+		maxSleepDuration: maxSleepDuration,
+		startTime:        time.Now(),
+	}
 }
 
 func (l *SlowSink) Sink(ctx context.Context, datumStreamCh <-chan sinksdk.Datum) sinksdk.Responses {
 	result := sinksdk.ResponsesBuilder()
+	min := l.minSleepDuration
+	max := l.maxSleepDuration
+
+	fmt.Println("min sleep time: ", min)
+	fmt.Println("max sleep time: ", max)
+
 	for d := range datumStreamCh {
 		id := d.ID()
-		if d.EventTime().Nanosecond()%3 == 0 {
-			sleepDuration := time.Duration(rand.Intn(10)) * time.Second
-			fmt.Println("id: ", id, "sleep time: ", sleepDuration)
+		// first 5 minutes(configurable), sink should work normally. Post that, based on event times of mssgs,
+		// if event time is multiple of 3 (configurable) we are intriducing sleep to mimic slow sink
+		// sleep duration is in a range based on env variables (min and max time)
+		if d.EventTime().Nanosecond()%3 == 0 && time.Since(l.startTime) >= 5*time.Minute {
+			randomNumber := rand.Intn(max-min+1) + min
+			sleepDuration := time.Duration(randomNumber) * time.Second
+			fmt.Println("id: ", id, "sleep time: ", sleepDuration, "event time: ", d.EventTime().Nanosecond())
 			time.Sleep(sleepDuration)
 		} else {
 			fmt.Println("id: ", id, "event time: ", d.EventTime().Nanosecond())
